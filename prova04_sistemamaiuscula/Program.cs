9d38cd5dd40a311540cd265747e113634e4b1701
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-09 18:45:12 -0400

    quetao4

---
+++ prova04_sistemamaiuscula/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prova04_sistemamaiuscula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome completo: ");
+            string fullname = Console.ReadLine().ToUpper();
+
+            Console.Clear();
+
+            Console.WriteLine("O nome informado foi: {0}", fullname);
+        }
+    }
+}
